f0b58d4fcee72abe058ef6e722ad1b5d4f4e767d
Yuri Titov <ytitov@gmail.com>
2020-07-12 22:42:14 -0500

    working

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,32 @@
 use serde_json as json;
 mod parts;
 
+use clap::Clap;
+
 use parts::*;
 
+#[derive(Clap, Debug)]
+#[clap(version = "1.0", author = "Yuri Titov <ytitov@gmail.com>")]
+struct Opts {
+    pub in_file: String,
+    pub out_folder: String,
+}
+
 fn main() {
+    let opts: Opts = Opts::parse();
+    let mut s = Schema::new(&opts.in_file, &opts.out_folder);
+    match s.process_file() {
+        Ok(_) => {
+        },
+        Err(e) => {
+            println!("Given: {:?}", &opts);
+            println!("there was an error: {:?}", e);
+        },
+    }
+}
+
+/*
+fn main_old() {
     let data = r#"
         {
             "name": "John Doe",
@@ -28,6 +51,8 @@ fn main() {
     let data2 = r#"
         {
             "name": "Mary Jane",
+            "extra_name": "MJ",
+            "age": 25,
             "phones": [
                 "402 444 1212"
             ],
@@ -38,25 +63,20 @@ fn main() {
             "features_extra": {
                 "extra": "air"
             },
-            "age": 25,
             "documents": {
                 "rent": { "pages": [7,8] },
                 "electricBill": { "pages": ["f", "q"] }
             }
         }"#;
-    let v: Element<String> = Element {
-        parent: None,
-        fk: None,
-        pk: 1,
-        key: Some(String::from("sample")),
-        value: String::from("example"),
-    };
     //let val = json::to_value(v);
     let val = json::from_str(data).expect("Bad json");
-    //let val2 = json::from_str(data2).expect("Bad json");
-    let mut s = Schema::new();
+    let val2 = json::from_str(data2).expect("Bad json");
+    let mut s = Schema::new(".");
     s.trav(0, None, vec![String::from("ROOT")], val);
+    //s.trav2(vec![String::from("ROOT")], val);
     //println!("\n\n{}", &s);
-    //s.trav(0, None, vec![String::from("ROOT")], val2);
+    s.trav(0, None, vec![String::from("ROOT")], val2);
     println!("\n\n{}", &s);
+    s.export_csv();
 }
+*/
--- src/parts.rs
+++ src/parts.rs
@@ -1,64 +1,100 @@
-use serde::{de, Deserialize, Deserializer, Serialize};
 use serde_json::Value;
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap};
 use std::fmt;
 
-#[derive(Deserialize, Serialize)]
-pub struct Element<V> {
-    pub parent: Option<String>,
-    pub fk: Option<usize>,
-    pub pk: usize,
-    pub value: V,
-    pub key: Option<String>,
-}
-
-/*
-pub fn t<'de, T, Q>(t: T) where T: Deserialize<'de> {
-}
-*/
+use std::io::{self, BufReader};
+use std::io::prelude::*;
+use std::fs::File;
 
 #[derive(Debug)]
 pub struct Table {
     pub name: String,
-    pub columns: HashMap<String, u16>,
-    pub rows: HashMap<usize, HashMap<String, Value>>,
+    pub columns: BTreeMap<String, u16>,
+    pub rows: BTreeMap<usize, BTreeMap<String, Value>>,
 }
 
 impl Table {
     pub fn new(name: &str) -> Self {
         Table {
             name: name.to_owned(),
-            columns: HashMap::new(),
-            rows: HashMap::new(),
+            columns: BTreeMap::new(),
+            rows: BTreeMap::new(),
         }
     }
 
     pub fn create_entry(&mut self, parent_table: Option<(usize, &str)>) -> usize {
         let pk = self.rows.len();
-        let vals = match parent_table {
-            Some((fk, parent_name)) => {
-                let col_name = format!("{}_id", parent_name);
-                self.columns.insert(col_name.clone(), 0);
-                let mut hs = HashMap::new();
-                hs.insert(col_name, Value::from(fk));
-                hs
-            }
-            _ => HashMap::new(),
-        };
-        self.rows.insert(pk, vals);
+        if let Some((fk, parent_name)) = parent_table {
+            let col_name = format!("{}_id", parent_name);
+            self.columns.insert(col_name.clone(), 0);
+            let mut hs = BTreeMap::new();
+            hs.insert(col_name, Value::from(fk));
+            self.rows.insert(pk, hs);
+        }
         pk
     }
 
-    pub fn set_row(&mut self, pk: usize, row: HashMap<String, Value>) {
+    pub fn set_row(&mut self, pk: usize, row: BTreeMap<String, Value>) {
         if let Some(existing_row) = self.rows.get_mut(&pk) {
             for (col, value) in row {
                 existing_row.entry(col).or_insert(value);
             }
+            for (key, _) in existing_row {
+                self.columns.entry(key.to_owned()).or_insert(0);
+            }
+        } else {
+            println!("set_row with pk: {} row: {:?} \ntable: {}", pk, row, self);
+            panic!("Tried to set row which does not exist");
         }
     }
 
-    pub fn add_row(&mut self, row: HashMap<String, Value>) {
+    pub fn add_row(&mut self, row: BTreeMap<String, Value>) {
+        for (key, _) in &row {
+            self.columns.entry(key.to_owned()).or_insert(0);
+        }
         self.rows.insert(self.rows.len(), row);
+    }
+
+    pub fn export_csv(self, export_path: &str) {
+        use std::io::prelude::*;
+        use std::path::Path;
+        let fname = format!("{}/{}.csv", export_path, &self.name);
+        let path = Path::new(&fname);
+        let display = path.display();
+
+        let mut file = match File::create(&path) {
+            Err(why) => panic!("couldn't create {}: {}", display, why),
+            Ok(file) => file,
+        };
+
+        // Write the `LOREM_IPSUM` string to `file`, returns `io::Result<()>`
+        let mut columns_str = format!("{}_id", self.name);
+        for (key, val) in &self.columns {
+            columns_str.push_str(",");
+            columns_str.push_str(&key);
+        }
+        columns_str.push_str("\n");
+        match file.write_all(columns_str.as_bytes()) {
+            Err(why) => panic!("couldn't write to {}: {}", display, why),
+            Ok(_) => (),
+        }
+
+        for (idx, row) in self.rows {
+            let mut line = format!("{}", idx);
+            for (col, _) in &self.columns {
+                if let Some(val) = row.get(col) {
+                    line.push_str(&format!(",{}", val));
+                } else {
+                    line.push_str(",");
+                }
+            }
+            line.push_str("\n");
+            match file.write_all(line.as_bytes()) {
+                Err(why) => panic!("couldn't write to {}: {}", display, why),
+                Ok(_) => (),
+            }
+        }
+        println!("successfully wrote to {}", display);
     }
 }
 
@@ -75,6 +111,8 @@ impl fmt::Display for Table {
 #[derive(Debug)]
 pub struct Schema {
     // key: (depth, table name)
+    pub input_file_path: String,
+    pub export_path: String,
     pub data: HashMap<String, Table>,
 }
 
@@ -88,9 +126,11 @@ impl fmt::Display for Schema {
 }
 
 impl Schema {
-    pub fn new() -> Self {
+    pub fn new(input_file_path: &str, export_path: &str) -> Self {
         Schema {
             data: HashMap::new(),
+            input_file_path: input_file_path.to_owned(),
+            export_path: export_path.to_owned(),
         }
     }
     pub fn create_entry(
@@ -110,7 +150,7 @@ impl Schema {
         }
     }
 
-    pub fn set_table_row(&mut self, tables: &[String], idx: usize, row: HashMap<String, Value>) {
+    pub fn set_table_row(&mut self, tables: &[String], idx: usize, row: BTreeMap<String, Value>) {
         let table_name = tables.join("_");
         if let Some(t) = self.data.get_mut(&table_name) {
             t.set_row(idx, row);
@@ -119,7 +159,7 @@ impl Schema {
         }
     }
 
-    pub fn add_table_row(&mut self, tables: &[String], row: HashMap<String, Value>) {
+    pub fn add_table_row(&mut self, tables: &[String], row: BTreeMap<String, Value>) {
         let table_name = tables.join("_");
         if let Some(t) = self.data.get_mut(&table_name) {
             t.add_row(row);
@@ -127,6 +167,36 @@ impl Schema {
             panic!("set_row could not find the table, by this point this should not happen");
         }
     }
+
+    /*
+    pub fn trav2(&mut self, parents: Vec<String>, val: Value) {
+        match val {
+            Value::Object(obj) => {
+                let mut row = BTreeMap::new();
+                for (key, value) in obj {
+                    match value {
+                        Value::Object(obj) => {
+                            let mut p = parents.clone();
+                            p.push(key.clone());
+                            row.insert(format!("{}_ID", parents.join("_")), Value::from(0));
+                            self.trav2(p, Value::from(obj));
+                        }
+                        Value::Array(arr) => {
+                        }
+                        other => {
+                            row.insert(parents.join("_"), Value::from(other));
+                        }
+                    }
+                }
+                println!("ROW: {:?}", &row);
+            }
+            Value::Array(arr) => {
+            }
+            other_value => {
+            }
+        }
+    }
+    */
 
     pub fn trav(
         &mut self,
@@ -138,7 +208,7 @@ impl Schema {
         match &val {
             Value::Object(o) => {
                 let fk = self.create_entry(parents_info, &parents);
-                let mut values = HashMap::new();
+                let mut values = BTreeMap::new();
                 //println!("Obj Create: {:?}", &parents);
 
                 for (key, val) in o {
@@ -160,8 +230,15 @@ impl Schema {
                     }
                 }
                 //println!("Table => {:?}", &parents);
-                //println!("  Row => {:?} FK: {:?}", &values, fk);
-                self.set_table_row(&parents, fk, values);
+                if values.len() > 0 {
+                    if let Some((pk, parent_name)) = parents_info {
+                        values.insert(format!("{}_id", parent_name), Value::from(pk));
+                        // the fk will only be valid if there was parents_info
+                        self.set_table_row(&parents, fk, values);
+                    } else {
+                        self.add_table_row(&parents, values);
+                    }
+                }
                 None
             }
             Value::Array(arr) => {
@@ -170,7 +247,7 @@ impl Schema {
 
                 let mut col_name = String::from("ERROR");
                 if parents.len() > 0 {
-                    col_name = String::from(&parents[parents.len()-1]);
+                    col_name = String::from(&parents[parents.len() - 1]);
                 }
                 for val in arr {
                     let v = self.trav(
@@ -186,7 +263,7 @@ impl Schema {
                             other => {
                                 //values.push(other.clone());
                                 //println!("array fk {} parent_info {:?}", fk, parents_info);
-                                let mut hs = HashMap::with_capacity(2);
+                                let mut hs = BTreeMap::new();
                                 //hs.insert(parents.join("_"), other);
                                 hs.insert(col_name.clone(), other);
                                 if let Some((pk, parent_name)) = parents_info {
@@ -201,9 +278,34 @@ impl Schema {
                 //println!("  Row => {:?}", &values);
                 None
             }
-            other => {
-                Some(other.to_owned())
+            other => Some(other.to_owned()),
+        }
+    }
+
+    pub fn export_csv(mut self) {
+        for (_, table) in self.data {
+            table.export_csv(&self.export_path);
+        }
+    }
+
+    pub fn process_file(mut self) -> std::result::Result<(), Box<dyn std::error::Error>> {
+        let f = File::open(&self.input_file_path)?;
+        let f = BufReader::new(f);
+
+        for line in f.lines() {
+            //println!("{}", line.unwrap());
+            match serde_json::from_str(&line?) {
+                Ok::<Value,_>(val) => {
+                    self.trav(0, None, vec![String::from("ROOT")], val);
+                }
+                Err(e) => {
+                    println!("ERROR: {:?}", e);
+                }
             }
         }
+
+        self.export_csv();
+
+        Ok(())
     }
 }
